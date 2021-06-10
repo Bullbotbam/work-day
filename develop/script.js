2f623467617e580c75b314d6317ac789cc9ed4e5
BullBot <dbullaustintx@gmail.com>
2021-06-10 18:27:37 -0500

    commented out sections

--- develop/script.js
+++ develop/script.js
@@ -1,4 +1,5 @@
-var today = new Date();
+// the current date and time
+var today = moment().format("dddd, MMMM Do YYYY, h:mm A");
 // var date =
 //   today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDay();
 document.getElementById("currentDay").innerHTML = today;
@@ -20,3 +21,9 @@ $(document).ready(function () {
 
   $("#container").append(divHour, divTime, inputNote, saveBtn);
 });
+// get current hour
+// if note is active during current hour background-color: red and disable input;
+// if note is post-active during current hour background-color: gray and enable input;
+// if note is pre-active during current hour background-color: green and enable input;
+// loop thorugh an array of objects (time: <value>, notes: <value>), save objects, skip if null
+// set results of array to localStorage
